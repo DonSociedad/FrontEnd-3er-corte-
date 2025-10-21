4da17c75216294a5f380adc19e0b84bc6cac7c2b
Isabela Taborda <isabelataborda2005@gmail.com>
2025-10-21 17:33:23 -0500

    se creo useRegister como hook

--- components/molecules/registerComponents.tsx
+++ components/molecules/registerComponents.tsx
@@ -1,48 +1,12 @@
 'use client'
 
-import { useForm, SubmitHandler } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { RegisterDTO } from "@/interfaces/register"
-import { RegisterScheme } from "@/schemas/register"
-
-import InputComponent from "@/components/atoms/inputComponents"
-import ButtonComponent from "@/components/atoms/buttonComponents"
-import { registerService } from "@/libs/authService"
-import { useRouter } from "next/navigation";
+import { useRegister } from "@/hooks/useRegister";
+import InputComponent from "../atoms/inputComponents";
+import ButtonComponent from "../atoms/buttonComponents";
 
 
 export default function RegisterComponent() {
-    const router = useRouter();
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm<RegisterDTO>({
-    });
-
-
-    const onSubmit: SubmitHandler<RegisterDTO> = async (data) => {
-    try {
-        const response = await registerService(data); 
-
-        // Si registerService devuelve un JSON con éxito:
-        if (response && !response.error) {
-        alert("✅ Registro exitoso. Redirigiendo al inicio de sesión...");
-        router.push("/login"); 
-        } else {
-        alert("Error al registrar usuario. Revisa los datos e inténtalo de nuevo.");
-        console.error("Error en el registro:", response);
-        }
-    } catch (error) {
-        console.error("Error inesperado en registerService:", error);
-        alert("Error de conexión con el servidor. Intenta más tarde.");
-    }
-    };
-
-    const onErrors=() => {
-        console.log("Errores encontrados en el momento de registro, vuelve a ver",errors);
-        alert("Información incompleta o errónea, vuelve a intentar");
-    };
+    const { register, handleSubmit, onSubmit, onErrors } = useRegister();
     
     return (
         <form
