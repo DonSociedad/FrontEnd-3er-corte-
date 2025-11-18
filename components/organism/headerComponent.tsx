e6256b915f214bb7752c536b805172139abda580
JeronimoRD <jeronimo.ramirezd24@gmail.com>
2025-11-18 13:53:57 -0500

    Se hookerizo el header

--- components/organism/headerComponent.tsx
+++ components/organism/headerComponent.tsx
@@ -1,22 +1,26 @@
-export default function HeaderComponent({ onNavigate }: { onNavigate: (page: string) => void }) {
+'use client';
+import { useHeader } from '@/hooks/useHeader';
+export default function HeaderComponent() {
+  const { onNavigate } = useHeader();
+
   return (
     <aside className="flex flex-col bg-gray-800 text-white w-64 h-screen p-6 fixed left-0 top-0">
       <div className="flex items-center justify-center mb-10">
-        <button onClick={() => onNavigate("workplace")} className="flex flex-col items-center">
+        <button onClick={() => onNavigate('workplace')} className="flex flex-col items-center">
           <img className="h-12 w-auto mb-2" src="/Piglance.png" alt="Piglance" />
         </button>
       </div>
 
       <nav className="flex flex-col gap-4 w-full">
         <button
-          onClick={() => onNavigate("workplace")}
+          onClick={() => onNavigate('workplace')}
           className="flex items-center gap-3 px-4 py-3 rounded-xl transition hover:bg-gray-600 font-bold text-white tracking-wide text-sm"
         >
           <span className="text-2xl">🏠</span> <span>APRENDER</span>
         </button>
 
         <button
-          onClick={() => onNavigate("profile")}
+          onClick={() => onNavigate('profile')}
           className="flex items-center gap-3 px-4 py-3 rounded-xl transition hover:bg-gray-600 font-bold text-white tracking-wide text-sm"
         >
           <span className="text-2xl">👤</span> <span>PERFIL</span>
